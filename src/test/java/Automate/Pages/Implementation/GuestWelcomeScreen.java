214e4c071168d699fd41b09e1d42260c639342d6
Eftiar <eftiar.hossain@monstar-lab.com>
2024-03-21 13:51:09 +0600

    updates

--- src/test/java/Automate/Pages/Implementation/GuestWelcomeScreen.java
+++ src/test/java/Automate/Pages/Implementation/GuestWelcomeScreen.java
@@ -22,20 +22,20 @@ public class GuestWelcomeScreen implements GuestWelcomeScreenVerification {
         Operations.verifyElementIsDisplayed(WelcomeScreenGuest_OR.Locator.signInLink, driver);
         Operations.matchText(WelcomeScreenGuest_OR.Locator.signInLink, WelcomeScreenGuest_OR.UIElementConstant.signInLink, driver);
 
-        if(Operations.verifyElementIsPresent(WelcomeScreenGuest_OR.Locator.rewardsCTA,driver)){
-            Operations.verifyElementIsDisplayed(WelcomeScreenGuest_OR.Locator.rewardsCTA, driver);
-            Operations.matchText(WelcomeScreenGuest_OR.Locator.rewardsCTA, WelcomeScreenGuest_OR.UIElementConstant.rewardsCTA, driver);
-            System.out.println("Element is present");
-        }
-        else
-            System.out.println("Rewards Element is not present");
-
-        if(Operations.verifyElementIsPresent(WelcomeScreenGuest_OR.Locator.inboxCTA,driver)){
-            Operations.verifyElementIsDisplayed(WelcomeScreenGuest_OR.Locator.inboxCTA, driver);
-            System.out.println("Element is present");
-        }
-        else
-            System.out.println("Inbox Element is not present");
+//        if(Operations.verifyElementIsPresent(WelcomeScreenGuest_OR.Locator.rewardsCTA,driver)){
+//            Operations.verifyElementIsDisplayed(WelcomeScreenGuest_OR.Locator.rewardsCTA, driver);
+//            Operations.matchText(WelcomeScreenGuest_OR.Locator.rewardsCTA, WelcomeScreenGuest_OR.UIElementConstant.rewardsCTA, driver);
+//            System.out.println("Element is present");
+//        }
+//        else
+//            System.out.println("Rewards Element is not present");
+//
+//        if(Operations.verifyElementIsPresent(WelcomeScreenGuest_OR.Locator.inboxCTA,driver)){
+//            Operations.verifyElementIsDisplayed(WelcomeScreenGuest_OR.Locator.inboxCTA, driver);
+//            System.out.println("Element is present");
+//        }
+//        else
+//            System.out.println("Inbox Element is not present");
 
         Operations.verifyElementIsDisplayed(WelcomeScreenGuest_OR.Locator.orderNowButton, driver);
         Operations.matchText(WelcomeScreenGuest_OR.Locator.orderNowButton, WelcomeScreenGuest_OR.UIElementConstant.orderNowButton, driver);
